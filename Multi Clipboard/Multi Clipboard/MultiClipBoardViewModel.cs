b48a8a08a18fd8b7b45741b49264af049089825c
Razonek <raging@onet.eu>
2018-05-17 17:24:46 +0200

    Caliburn.Micro

---
+++ Multi Clipboard/Multi Clipboard/MultiClipBoardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Caliburn.Micro;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Clipboard
+{
+    public class MultiClipBoardViewModel : Screen
+    {
+        public MultiClipBoardViewModel()
+        {
+            this.DisplayName = "Multi Clipboard";
+        }
+
+
+    }
+
+
+
+
+
+}
